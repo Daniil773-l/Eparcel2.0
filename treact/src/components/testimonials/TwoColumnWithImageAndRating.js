2774a05541973d50bcd750dcee917b5d162ab965
Shek_Dan <83953260+Daniil773-l@users.noreply.github.com>
2024-06-24 19:02:14 +0500

    Reg

--- treact/src/components/testimonials/TwoColumnWithImageAndRating.js
+++ treact/src/components/testimonials/TwoColumnWithImageAndRating.js
@@ -14,16 +14,15 @@ const Row = tw.div`flex flex-col md:flex-row justify-between items-center`;
 const Column = tw.div`w-full max-w-md mx-auto md:max-w-none md:mx-0`;
 const ImageColumn = tw(Column)`md:w-5/12 xl:w-6/12 flex-shrink-0 relative`;
 const TextColumn = styled(Column)(props => [
-  tw`md:w-7/12 xl:w-6/12 mt-16 md:mt-0`,
-  props.textOnLeft ? tw`md:pr-12 lg:pr-16 md:order-first` : tw`md:pl-12 lg:pl-16 md:order-last`
+    tw`md:w-7/12 xl:w-6/12 mt-16 md:mt-0`,
+    props.textOnLeft ? tw`md:pr-12 lg:pr-16 md:order-first` : tw`md:pl-12 lg:pl-16 md:order-last`
 ]);
 
 const Image = styled.img(props => [
-  props.imageRounded && tw`rounded`,
-  props.imageBorder && tw`border`,
-  props.imageShadow && tw`shadow`,
-  { width: props.imageWidth ? `${props.imageWidth}px` : 'auto'}, // Apply width dynamically
-  { height: props.imageHeight ? `${props.imageHeight}px` : 'auto'} // Apply height dynamically
+    props.imageRounded && tw`rounded`,
+    props.imageBorder && tw`border`,
+    props.imageShadow && tw`shadow`,
+    tw`max-w-full h-auto`, // Make the image responsive
 ]);
 
 const Subheading = tw(SubheadingBase)`text-center md:text-left`;
@@ -33,13 +32,13 @@ const Heading = tw(
 const Description = tw.p`mt-6 text-center md:text-left text-sm md:text-base lg:text-lg font-medium leading-relaxed text-secondary-100`;
 
 const TestimonialSlider = styled(Slider)`
-  ${tw`w-full mt-10 text-center md:text-left`}
-  .slick-track {
-    ${tw`flex`}
-  }
-  .slick-slide {
-    ${tw`h-auto flex justify-center mb-1`}
-  }
+    ${tw`w-full mt-10 text-center md:text-left`}
+    .slick-track {
+        ${tw`flex`}
+    }
+    .slick-slide {
+        ${tw`h-auto flex justify-center mb-1`}
+    }
 `;
 
 const Testimonial = tw.div`outline-none h-full flex! flex-col`;
@@ -54,10 +53,10 @@ const CustomerName = tw.h5`font-bold text-xl`;
 const CustomerTitle = tw.p`font-medium text-secondary-100`;
 
 const Controls = styled.div`
-  ${tw`flex mt-8 sm:mt-0`}
-  .divider {
-    ${tw`my-3 border-r`}
-  }
+    ${tw`flex mt-8 sm:mt-0`}
+    .divider {
+        ${tw`my-3 border-r`}
+    }
 `;
 const ControlButton = styled.button`
     ${tw`mx-3 p-4 rounded-full transition duration-300 bg-gray-200 hover:bg-gray-300 text-primary-500 hover:text-primary-700 focus:outline-none focus:shadow-outline`}
@@ -75,77 +74,77 @@ const ControlButton = styled.button`
 
 
 export default ({
-                  imageSrc = loveIllustrationImageSrc,
-                  imageRounded = true,
-                  imageBorder = false,
-                  imageShadow = false,
-                  imageWidth = 640, // example width in pixels
-                  imageHeight = 640, // example height in pixels
-                  subheading = "Testimonials",
-                  heading = "Our Clients Love Us.",
-                  textOnLeft = false,
-                  testimonials = [
-                    {
-                      stars: 5,
+                    imageSrc = loveIllustrationImageSrc,
+                    imageRounded = true,
+                    imageBorder = false,
+                    imageShadow = false,
+                    imageWidth = 640, // example width in pixels
+                    imageHeight = 640, // example height in pixels
+                    subheading = "Testimonials",
+                    heading = "Our Clients Love Us.",
+                    textOnLeft = false,
+                    testimonials = [
+                        {
+                            stars: 5,
 
-                      heading: "Где покупать товары с выгодой",
-                      quote:
-                          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
-                      customerName: "Charlotte Hale",
-                      customerTitle: "CEO, Delos Inc."
-                    },
-                    {
-                      stars: 5,
-                      profileImageSrc:
-                          "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=512&h=512&q=80",
-                      heading: "Love the Developer Experience and Design Principles !",
-                      quote:
-                          "Sinor Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-                      customerName: "Adam Cuppy",
-                      customerTitle: "Founder, EventsNYC"
-                    }
-                  ]
+                            heading: "Где покупать товары с выгодой",
+                            quote:
+                                "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+                            customerName: "Charlotte Hale",
+                            customerTitle: "CEO, Delos Inc."
+                        },
+                        {
+                            stars: 5,
+                            profileImageSrc:
+                                "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=512&h=512&q=80",
+                            heading: "Love the Developer Experience and Design Principles !",
+                            quote:
+                                "Sinor Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+                            customerName: "Adam Cuppy",
+                            customerTitle: "Founder, EventsNYC"
+                        }
+                    ]
                 }) => {
-  const [sliderRef, setSliderRef] = useState(null);
+    const [sliderRef, setSliderRef] = useState(null);
 
-  return (
-      <Container>
-        <ContentWithPaddingXl>
-          <Row>
-            <ImageColumn>
-              <Image src={imageSrc} imageBorder={imageBorder} imageShadow={imageShadow} imageRounded={imageRounded} style={{ width: `${imageWidth}px`, height: `${imageHeight}px` }} />
-            </ImageColumn>
-            <TextColumn textOnLeft={textOnLeft}>
-              <Heading>{heading}</Heading>
-              <TestimonialSlider arrows={false} ref={setSliderRef}>
-                {testimonials.map((testimonial, index) => (
-                    <Testimonial key={index}>
-                      <TestimonialHeading>{testimonial.heading}</TestimonialHeading>
-                      <Quote>{testimonial.quote}</Quote>
-                      <CustomerInfoAndControlsContainer>
-                        <CustomerInfo>
+    return (
+        <Container>
+            <ContentWithPaddingXl>
+                <Row>
+                    <ImageColumn>
+                        <Image src={imageSrc} imageBorder={imageBorder} imageShadow={imageShadow} imageRounded={imageRounded} style={{ width: '100%', height: 'auto' }} />
+                    </ImageColumn>
+                    <TextColumn textOnLeft={textOnLeft}>
+                        <Heading>{heading}</Heading>
+                        <TestimonialSlider arrows={false} ref={setSliderRef}>
+                            {testimonials.map((testimonial, index) => (
+                                <Testimonial key={index}>
+                                    <TestimonialHeading>{testimonial.heading}</TestimonialHeading>
+                                    <Quote>{testimonial.quote}</Quote>
+                                    <CustomerInfoAndControlsContainer>
+                                        <CustomerInfo>
 
-                          <CustomerTextInfo>
-                            <CustomerName>{testimonial.customerName}</CustomerName>
-                            <CustomerTitle>{testimonial.customerTitle}</CustomerTitle>
-                          </CustomerTextInfo>
-                        </CustomerInfo>
-                        <Controls>
-                          <ControlButton onClick={sliderRef?.slickPrev}>
-                            <ArrowLeftIcon />
-                          </ControlButton>
-                          <div className="divider" />
-                          <ControlButton onClick={sliderRef?.slickNext}>
-                            <ArrowRightIcon />
-                          </ControlButton>
-                        </Controls>
-                      </CustomerInfoAndControlsContainer>
-                    </Testimonial>
-                ))}
-              </TestimonialSlider>
-            </TextColumn>
-          </Row>
-        </ContentWithPaddingXl>
-      </Container>
-  );
+                                            <CustomerTextInfo>
+                                                <CustomerName>{testimonial.customerName}</CustomerName>
+                                                <CustomerTitle>{testimonial.customerTitle}</CustomerTitle>
+                                            </CustomerTextInfo>
+                                        </CustomerInfo>
+                                        <Controls>
+                                            <ControlButton onClick={sliderRef?.slickPrev}>
+                                                <ArrowLeftIcon />
+                                            </ControlButton>
+                                            <div className="divider" />
+                                            <ControlButton onClick={sliderRef?.slickNext}>
+                                                <ArrowRightIcon />
+                                            </ControlButton>
+                                        </Controls>
+                                    </CustomerInfoAndControlsContainer>
+                                </Testimonial>
+                            ))}
+                        </TestimonialSlider>
+                    </TextColumn>
+                </Row>
+            </ContentWithPaddingXl>
+        </Container>
+    );
 };
